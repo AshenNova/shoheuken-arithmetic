5a328fda0798cdb937a1bfd27d3092dc4988bc6c
AshenNova <Kennerve14@gmail.com>
2021-09-15 13:39:32 +0800

    arithmetic prototype

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,62 @@
-let currentScore = 0;
-let n1 = Math.floor(Math.random()*10+1)
-let n2 = Math.floor(Math.random()*10+1)
+const displayProblem = document.querySelector(".display-problems")
+const ourForm = document.querySelector(".our-form")
+const userInput = document.getElementById("user-input")
+const currentScore = document.getElementById("current-score")
+const currentMistake = document.getElementById("current-mistake")
 
-document.getElementById("box1text"). value = n1;
-document.getElementById("box2text"). value = n2;
+// Storing of question
+let state = {
+  score: 0,
+  mistake: 0
+}
 
-let answer = n1 + n2;
+// 1. generate new problem and store the question in state object
+// 2. Also to visual update the HTML
+function updateProblems(){
+  state.currentProblem = genProblems()
+  const p = state.currentProblem
+  displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`
+  userInput.value = ""
+  userInput.focus()
+}
 
-function Game(){
-  const user = document.getElementById("anstext").value
+updateProblems()
 
-  if (user == answer){
-    currentScore += 1
-    document.getElementById("current-score").innerHTML = currentScore
-  } else {
-    document.getElementById("current-score").innerHTML = "Incorrect ):"
+ourForm.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e){
+  e.preventDefault()
+  if (userInput.value == "") alert("Please input a value")
+  let correctAnswer
+  const p = state.currentProblem
+  if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+  if (p.operator == "-") correctAnswer = p.numOne - p.numTwo
+  console.log(correctAnswer, userInput.value)
+    if (parseInt(userInput.value,10) == correctAnswer){
+      state.score++
+      currentScore.textContent = state.score
+      console.log("correct")
+      updateProblems()
+      console.log("new questions generated")
+    } else {
+      console.log("incorrect")
+      state.mistake++
+      currentMistake.textContent = state.mistake
+      userInput.value = ""
+    }
+  
+}
+
+// generate number
+function genNumbers(max){
+  return Math.trunc(Math.random()*max)
+}
+
+// generate problem
+function genProblems(){
+  return{
+    numOne: genNumbers(10)-4,
+    numTwo: genNumbers(11),
+    operator: ["+","-"][genNumbers(2)]
   }
-document.getElementById("anstext").value = ""
-
-n1 = Math.floor(Math.random()*10+1)
-n2 = Math.floor(Math.random()*10+1)
-
-document.getElementById("box1text"). value = n1;
-document.getElementById("box2text"). value = n2;
-
-answer = n1 + n2;
 }
