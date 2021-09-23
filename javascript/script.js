7248387cedbf6a101c1bc17725635ecd1b902a3a
ashennova <kennerve14@gmail.com>
2021-09-23 22:09:30 +0800

    added settings and other levels

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,19 @@ const timerD = document.getElementById('timerD');
 const finalBox = document.querySelector('.finalBox');
 const finalText = document.getElementById('finalText');
 
+const levelSetting = document.querySelector('.level-setting');
+const level1 = document.querySelector('.level1');
+const level1dot2 = document.querySelector('.level1Dot2');
+
+const level2 = document.querySelector('.level2');
+const level3 = document.querySelector('.level3');
+const level4 = document.querySelector('.level4');
+const level5 = document.querySelector('.level5');
+const level6 = document.querySelector('.level6');
+const level7 = document.querySelector('.level7');
+
+let level = 0;
+
 // Storing of question
 let state = {
   score: 0,
@@ -22,7 +35,7 @@ function clickStart(){
   startBox.classList.add('hidden');
   countDownTimer.classList.remove('hidden');
     
-  // Timer1
+  // Timer1 countdown
     let time = 3;
     const countDownOne = setInterval(function(){
       
@@ -72,7 +85,45 @@ buttonStart.addEventListener('click', clickStart)
 function updateProblems(){
   state.currentProblem = genProblems()
   const p = state.currentProblem
-  displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`
+
+  // generating display -> Turn this into a function!
+  if (level == 1){
+    if (p.numOne >= p.numTwo){
+      displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`  
+    } else {
+      displayProblem.innerHTML = `${p.numTwo} ${p.operator} ${p.numOne}`
+    }
+  }
+  if (level == 1.2){
+    if (p.operator == "x" ) displayProblem.innerHTML = `${p.numThree} ${p.operator} ${p.numFour}`
+    if (p.operator == "÷") displayProblem.innerHTML = `${p.numThree*p.numFour} ${p.operator} ${p.numThree}` 
+  }
+  if (level == 2){
+    if (p.numOne >= p.numTwo){
+      displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`  
+    } else {
+      displayProblem.innerHTML = `${p.numTwo} ${p.operator} ${p.numOne}`
+    }
+  }
+
+  if (level == 3){
+    console.log(p.operator);
+    if (p.operator == "x" ) displayProblem.innerHTML = `${p.numThree} ${p.operator} ${p.numFour}` 
+    if (p.operator == "+") displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`
+    if (p.operator == "-") {
+      if (p.numOne >= p.numTwo){
+        displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`  
+      } else {
+        displayProblem.innerHTML = `${p.numTwo} ${p.operator} ${p.numOne}`
+      }
+    }
+    if (p.operator == "÷") displayProblem.innerHTML = `${p.numThree*p.numFour} ${p.operator} ${p.numThree}` 
+  }
+
+  if (level == 7){
+    displayProblem.innerHTML = `${p.numOne} ${p.operator} ${p.numTwo}`
+  }
+
   userInput.value = ""
   userInput.focus()
 }
@@ -85,9 +136,56 @@ function handleSubmit(e){
   e.preventDefault()
   if (userInput.value == "") alert("Please input a value")
   let correctAnswer
+  // let divAnswer
   const p = state.currentProblem
-  if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
-  if (p.operator == "-") correctAnswer = p.numOne - p.numTwo
+  
+  // Determining answer -> Turn this into a function!
+    if (level == 1){
+      if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+      if (p.operator == "-") {
+        if (p.numOne >= p.numTwo) {
+          correctAnswer = p.numOne - p.numTwo
+        } else {
+          correctAnswer = p.numTwo - p.numOne
+        }
+      }        
+    }
+
+    if (level == 1.2){
+      if (p.operator == "x") correctAnswer = p.numThree * p.numFour
+      if (p.operator == "÷") correctAnswer = (p.numThree*p.numFour)/p.numThree
+    }
+
+    if (level == 2){
+      if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+      if (p.operator == "-") {
+        if (p.numOne >= p.numTwo) {
+          correctAnswer = p.numOne - p.numTwo
+        } else {
+          correctAnswer = p.numTwo - p.numOne
+        }
+      }        
+    }
+
+    if (level == 3){
+      console.log(p.numOne, p.numTwo, p.numThree, p.numFour, p.operator)
+      if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+      if (p.operator == "-") {
+        if (p.numOne >= p.numTwo) {
+          correctAnswer = p.numOne - p.numTwo
+        } else {
+          correctAnswer = p.numTwo - p.numOne
+        }
+      } 
+      if (p.operator == "x") correctAnswer = p.numThree * p.numFour
+      if (p.operator == "÷") correctAnswer = (p.numThree*p.numFour)/p.numThree
+    }
+
+    if (level == 7){
+      if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+      if (p.operator == "-") correctAnswer = p.numOne - p.numTwo
+    }
+
   console.log(correctAnswer, userInput.value)
     if (parseInt(userInput.value,10) == correctAnswer){
       console.log("correct")
@@ -115,9 +213,96 @@ function genNumbers(max){
 
 // generate problem
 function genProblems(){
-  return{
-    numOne: genNumbers(10)-4,
-    numTwo: genNumbers(11),
-    operator: ["+","-"][genNumbers(2)]
+  console.log(level);
+
+  if (level == 1){
+    return {
+      numOne: genNumbers(5),
+      numTwo: genNumbers(5),
+      operator: ["+","-"][genNumbers(2)]
+    }
+  }
+
+  if (level == 1.2){
+    return {
+      numThree: genNumbers(5)+1,
+      numFour: genNumbers(4)+1,
+      operator: ["x","÷"][genNumbers(2)]
+    }
+  }
+
+  if (level == 2){
+    return {
+      numOne: genNumbers(50),
+      numTwo: genNumbers(50)+10,
+      operator: ["+","-"][genNumbers(2)]
+    }
+  }
+  if (level == 3){
+    return {
+      numOne: genNumbers(50),
+      numTwo: genNumbers(50)+10,
+      numThree: genNumbers(10)+1,
+      numFour: genNumbers(10)+1,
+      operator: ["+","-","x","÷"][genNumbers(4)]
+    }
+  }
+  if (level == 7){
+    return {
+      numOne: genNumbers(10)-4,
+      numTwo: genNumbers(11),
+      operator: ["+","-"][genNumbers(2)]
+    }  
   }
 }
+
+// function genAnswer(){
+//   if (level == 7){
+//     if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+//     if (p.operator == "-") correctAnswer = p.numOne - p.numTwo
+//   }
+//   if (level == 1){
+//     if (p.operator == "+") correctAnswer = p.numOne + p.numTwo
+//     if (p.operator == "-") {
+//       if (p.numOne >= p.numTwo) {
+//         correctAnswer = p.numOne - p.numTwo
+//       } else {
+//         correctAnswer = p.numTwo - p.numOne
+//       }
+//     }        
+//   }
+// }
+
+level1.addEventListener('click', function(){
+  level = 1
+  console.log('Level 1 selected')
+  levelSetting.classList.add('hidden')
+  startBox.classList.remove('hidden')
+})
+level1dot2.addEventListener('click', function(){
+  level = 1.2
+  console.log('Level 1.2 selected')
+  levelSetting.classList.add('hidden')
+  startBox.classList.remove('hidden')
+})
+
+level2.addEventListener('click', function(){
+  level = 2
+  console.log('Level 2 selected')
+  levelSetting.classList.add('hidden')
+  startBox.classList.remove('hidden')
+})
+
+level3.addEventListener('click', function(){
+  level = 3
+  console.log('Level 3 selected')
+  levelSetting.classList.add('hidden')
+  startBox.classList.remove('hidden')
+})
+
+level7.addEventListener('click', function(){
+  level = 7
+  console.log('Level 7 selected')
+  levelSetting.classList.add('hidden')
+  startBox.classList.remove('hidden')
+})
