7d695c81ecdc81e617c719f48fd5e6ccadd7470a
ashennova <kennerve14@gmail.com>
2022-02-15 17:21:20 +0800

    changed back 2.4

--- javascript/script.js
+++ javascript/script.js
@@ -987,7 +987,7 @@ for (let i = 0; i <  settingButton.length; i++){
 
     case "Level 2.4":
       level = 2.4;
-      scoreNeeded = 1;
+      scoreNeeded = 50;
       highScoreName.innerHTML = highScore2dot4.name
       highScoreTime.innerHTML = highScore2dot4.time
       highScoreMistakes.innerHTML = highScore2dot4.mistake
