3d052840cce45ff2c8436e4fee56bb3519c88bb8
ashennova <kennerve14@gmail.com>
2022-07-16 09:39:03 +0800

    adjusted 'leveltocap'

--- javascript/script.js
+++ javascript/script.js
@@ -6663,7 +6663,7 @@ function handleSubmit(e){
     console.log(correctAnswer, userInput.value)
 
       // adjustment
-      levelToCaps = [3.18, 4.17, 5, 5.06]
+      levelToCaps = [3.18, 4.17, 4.18, 5, 5.06]
       if (levelToCaps.includes(level) || (level == 3.16 && p.optionFinal == 4)){
         userInput.value = userInput.value.toUpperCase()
       }
