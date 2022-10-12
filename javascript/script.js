8e58c0871a29b9f13f1a8b1f8ffcf79906262476
ashennova <kennerve14@gmail.com>
2022-10-12 13:09:16 +0800

    KISS heu display

--- javascript/script.js
+++ javascript/script.js
@@ -5698,7 +5698,6 @@ displayProblem.innerHTML = `1 + 2 + 3 ... ... + ${p.numOne-2} + ${p.numOne-1} + 
   if ( level == "heuThree"){
 
     console.log(setting)
-    levelLabel.innerHTML = `You are attempting Level ${level}${setting == undefined ? "" : `.${setting}`}`  
     if ( setting == 1 || (setting == 9 && p.rollz == 1) || range == 1 && p.rollz == 1 ){
       while (p.numOne == p.numTwo) {
         p.numOne = (genNumbers(5)+1)*2
@@ -6134,7 +6133,6 @@ displayProblem.innerHTML = `1 + 2 + 3 ... ... + ${p.numOne-2} + ${p.numOne-1} + 
                               // display
 
   if ( level == "heuFour"){
-    levelLabel.innerHTML = `You are attempting Level ${level}${setting == undefined ? "" : `.${setting}`}`  
     if ( setting == 1 || setting == 9 && p.rollz == 1 || range == 1 && p.rollz == 1 ){
       
       while (p.objectOneQ == p.objectTwoQ || p.objectOneQ > p.objectTwoQ){
@@ -6345,7 +6343,6 @@ displayProblem.innerHTML = `1 + 2 + 3 ... ... + ${p.numOne-2} + ${p.numOne-1} + 
   }
                                 // Display
   if ( level == "heuFive"){
-    levelLabel.innerHTML = `You are attempting Level ${level}${setting == undefined ? "" : `.${setting}`}`
     if (setting == 1 || setting == 9 && p.rollz == 1 || range == 1 && p.rollz == 1 ){
       while (p.quantityOne == p.quantityTwo){
         p.quantityOne = genNumbers(10)+1
@@ -11094,13 +11091,20 @@ function genProblems(){
 
 }
 
+function levelDisplay(){
+  levelLabel.innerHTML = `You are attempting Level ${level}`
+  if (typeof level == "string"){
+    levelLabel.innerHTML = `You are attempting Level ${level}${setting == undefined ? "" : `.${setting}`}`
+  }
+}
+
 function levelBox(){
     scoreNeededCl.textContent = scoreNeeded;
     console.log(`Level ${level} selected`)
     levelSetting.classList.add('hidden')
     multiplesSettingCl.classList.add('hidden');
     startBox.classList.remove('hidden')
-    levelLabel.innerHTML = `You are attempting Level ${level}`
+    levelDisplay()
 }
 
 // function draw(){
