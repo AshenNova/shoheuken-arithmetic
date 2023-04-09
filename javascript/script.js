c9cddad499c83857dd9e7d7ff9e1580b4fbcf795
ashennova <kennerve14@gmail.com>
2023-04-09 12:45:11 +0800

    fixed cal2

--- javascript/script.js
+++ javascript/script.js
@@ -7145,7 +7145,7 @@ function updateProblems() {
       let arrTwo = p.numTwo.toString().split("");
       let join = [...arrOne, ...arrTwo];
       let unique = [...new Set(join)];
-      list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      let list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
       console.log(unique, list);
       unique.forEach((el) => {
         console.log(el);
