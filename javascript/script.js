80ea81dc9549aa15ba224bad4380f202ee82514d
ashennova <kennerve14@gmail.com>
2023-06-27 18:42:04 +0800

    fixed extraPracticeArr

--- javascript/script.js
+++ javascript/script.js
@@ -24332,7 +24332,7 @@ function buttonLevelSetting() {
 
     case "Cal.4":
       level = "calFour";
-      scoreNeeded = 10;
+      scoreNeeded = 2;
       setting = prompt(
         "What level?\n1. Common Multiples\n2. Listing Factors\n3. Common Factors\n4. Double Digit Multiplication\n5. Left Side Right Side + - x /\n6. Fractions: Addition: Mixed Fractions\n7. Fractions: Subtraction: Mixed Fractions\n8. Decimals: Addition\n9. Decimals: Subtraction\n10. Decimals: Overlapping Place Value\n11. Decimals: Multiplication (Single)\n12. Decimals: Multiplication (Double)\n13. Decimals: Division \n14. Fractions to Decimal (Limit)\n15. Decimals: Division and Multiplication with splitting\n16. Multiplication in Sets\n17. Fractions: Unit with a Value\n\n99. Everything",
         99
@@ -24738,7 +24738,14 @@ extraPracticeBtn.addEventListener("click", function () {
   state.mistake = 0;
   currentScore.textContent = 0;
   currentMistake.textContent = 0;
-  calArr = extraPracticeArr;
+  // const cloneArr = extraPracticeArr;
+  calArr = []
+  for (let i = 0; i < 3; i++) {
+    extraPracticeArr.forEach((item) => {
+      calArr.push(item);
+    });
+  }
+  console.log(calArr);
   extraPracticeArr = [];
   player = 1;
   console.log(scoreNeeded);
